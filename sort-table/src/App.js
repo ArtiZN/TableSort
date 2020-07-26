c984aea4a2e81e4c1b5ee125c1f00f30a9e76610
KPbICA-CTATUCT <z.artemznahar@mail.ru>
2020-07-26 19:14:02 +0300

    ssssssssssssssssssss

--- sort-table/src/App.js
+++ sort-table/src/App.js
@@ -10,18 +10,24 @@ function App() {
     {id: 2, name: 'Pavel', salary: 20000, isMarried: true, dick: 42},
     {id: 3, name: 'Bill', salary: 3000000000, isMarried: true, dick: 14},
     {id: 4, name: 'Elon', salary: 1500000000, isMarried: true, dick: 13},
-    {id: 5, name: 'Artem', salary: 0, isMarried: false, dick: 0}
+    {id: 5, name: 'Mark', salary: 980, isMarried: true, dick: 12},
+    {id: 6, name: 'Victor', salary: 0, isMarried: false, dick: 0},
+    {id: 7, name: 'Artem', salary: 3, isMarried: true, dick: 4},
+    {id: 8, name: 'Andrew', salary: 5, isMarried: false, dick: 3},
+    {id: 9, name: 'Gay', salary: 10, isMarried: true, dick: 4},
+    {id: 10, name: 'Van', salary: 65, isMarried: false, dick: 7},
   ])
-
-  
 
   const [buttonPressed, setButtonPressed] = useState(false)
   const mymusic = new Audio(music)
   
   useEffect(()=>{
+    if (buttonPressed) {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      mymusic.play()
+    }
+    else mymusic.pause()  
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    if (buttonPressed) mymusic.play()
-    else stopAudio()
   },[buttonPressed])
 
   const Styles = {
@@ -48,7 +54,7 @@ function App() {
     if (buttonPressed) return(
       <>
       <div className='trap'>
-        <img src="https://media1.tenor.com/images/ffc86c2754e1bc3c8c2ac8ff572b0766/tenor.gif?itemid=16690874" className='gif'/>
+        <img src="https://media1.tenor.com/images/ffc86c2754e1bc3c8c2ac8ff572b0766/tenor.gif?itemid=16690874" className='gif' alt="Oh shit I'm sorry"/>
         
       </div>
       <button onClick={onClicked} style={Styles.dangerousButton}> Stop this </button>
--- sort-table/src/utils/TableContent.js
+++ sort-table/src/utils/TableContent.js
@@ -1,7 +1,26 @@
 import React from 'react' 
 
-
  export default props => (
-     <div></div>
+     props.data.map(item =>{
+        return <div className='row'>
+            <div id='column'>
+                {item.id}
+            </div>
+            <div id='column'>
+                {item.name}
+            </div>
+            <div id='column'>
+                {item.salary}
+            </div>
+            <div id='column'>
+                {item.isMarried ?
+                <div>Yes</div> : <div>No</div>}
+            </div>
+            <div id='column'>
+                {item.dick}
+            </div>
+        </div>
+     })
+     
  )
     
